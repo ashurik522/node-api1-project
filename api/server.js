b43285524cbd5f381fb5516ac0cfc62af1119124
ashurik522 <ashurik522@gmail.com>
2022-07-11 15:50:49 -0400

    through all tests, update functional

--- api/server.js
+++ api/server.js
@@ -39,6 +39,36 @@ server.post ('/api/users', (req, res) => {
     })
 })
 
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    Users.remove(id).then(user => {
+        if(user == null){
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else {
+            res.json(user)
+        }
+    })
+})
+
+server.put('/api/users/:id', (req, res) =>{
+    let body = req.body;
+
+    if(body.name == null || body.bio == null) {
+        res.status(400).json({ message: "Please provide name and bio for the user"})
+        return;
+    } 
+    
+    Users.update(req.params.id, body).then(user => {
+        if(user == null){
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+        } else {
+            res.status(200).json(user)
+        }
+        
+    })
+})
+
 
 
 
