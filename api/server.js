5a63c0393d97d896cc1245057595609a3305e022
ashurik522 <ashurik522@gmail.com>
2022-07-11 15:27:00 -0400

    find by id

--- api/server.js
+++ api/server.js
@@ -1,9 +1,34 @@
 // BUILD YOUR SERVER HERE
+const Users = require('./users/model')
+
 const express = require('express')
 
 const server = express()
 
 server.use(express.json())
+
+
+//Endpoints
+
+//gets all users
+server.get('/api/users', (req, res) => {
+    Users.find().then(users => res.json(users))
+})
+
+//gets users by id
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+    Users.findById(id).then(user => {
+        if(user == null){
+            res.status(404).json({ message:'User not found'})
+        } else {
+            res.json(user)
+        }
+    })
+})
+
+
 
 
 
